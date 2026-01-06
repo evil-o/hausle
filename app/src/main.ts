f0461e004f93d7328ca3c5287d95c67bf7964577
Oliver Lomp <ol.lomp@gmail.com>
2026-01-06 22:41:14 +0100

    chore: add bootstrap

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 
 app.use(createPinia())

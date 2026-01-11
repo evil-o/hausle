f26919403f4474d9c55d719afccea336af6425f8
Oliver Lomp <ol.lomp@gmail.com>
2026-01-11 17:34:15 +0100

    ci: enable cypress tests

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://localhost:4200',
     supportFile: 'support/e2e.ts'
   },
 })

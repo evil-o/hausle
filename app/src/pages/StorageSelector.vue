f3ef850d0bc0b0dd475637c67185bf3898b56482
Oliver Lomp <ol.lomp@gmail.com>
2026-01-15 22:59:32 +0100

    chore: basic storage selector structure

---
+++ app/src/pages/StorageSelector.vue
@@ -0,0 +1,3 @@
+<template>
+    storage selector
+</template>
--- app/src/pages/WelcomePage.vue
+++ app/src/pages/WelcomePage.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { useStorageStore } from "@/stores/storage"
 import { StorageProvider } from '@/models/storage-provider';
+import StorageSelector from "./StorageSelector.vue";
 
 const storage = useStorageStore();
 </script>
 
 <template>
-    <div v-if="storage.chosenProvider === StorageProvider.None">
-      <p>No storage provider selected.</p>
-    </div>
+  <div v-if="storage.chosenProvider === StorageProvider.None">
+    <p>No storage provider selected.</p>
+    <BButton v-b-modal.select-storage-provider>Select storage provider</BButton>
+
+    <b-modal id="select-storage-provider" title="Select storage provider">
+      <StorageSelector></StorageSelector>
+    </b-modal>
+  </div>
 </template>
 
 <style scoped></style>

13831fc7322ea5cc2d032f5453a4e0e9399678dc
Oliver Lomp <ol.lomp@gmail.com>
2026-01-25 16:48:18 +0100

    test: make app test shallow

--- app/src/__tests__/App.spec.ts
+++ app/src/__tests__/App.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
   it('mounts renders properly', () => {
-    const wrapper = mount(App)
+    const wrapper = shallowMount(App)
     expect(wrapper).toBeTruthy()
   })
 })

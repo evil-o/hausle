363d7bc8af0d07f6b470523681de15ffc6e1685e
Oliver Lomp <ol.lomp@gmail.com>
2026-01-25 16:24:18 +0100

    chore: buttons for storage provider selection

--- app/src/pages/StorageSelector.vue
+++ app/src/pages/StorageSelector.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { useStorageStore } from "@/stores/storage"
+import { StorageProvider } from '@/models/storage-provider';
+
+const storage = useStorageStore();
+</script>
+
 <template>
     storage selector
+
+    <BRow class="mb-3">
+        <BCol>
+            <BButton v-on:click="storage.setProvider(StorageProvider.LocalStorage)">Local Storage</BButton>
+        </BCol>
+    </BRow>
+    <BRow>
+        <BCol>
+            <BButton v-on:click="storage.setProvider(StorageProvider.GoogleDrive)">Google Drive</BButton>
+        </BCol>
+    </BRow>
 </template>

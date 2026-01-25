511a3e5f43f896b96f3c95f635834d419bed4be0
Oliver Lomp <ol.lomp@gmail.com>
2026-01-25 22:52:54 +0100

    feat: meter card mockup

---
+++ app/src/components/MeterCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const meterId = ref("Meter id");
+const meterName = ref("Meter name");
+const lastReading = ref(1234);
+const unit = ref("kW");
+
+</script>
+
+<template>
+    <BCard no-body>
+        <BCardHeader>{{ meterId }}</BCardHeader>
+        <BCardBody>
+            <BCardTitle>
+                <BCardText>{{ meterName }}</BCardText>
+            </BCardTitle>
+            <BCardText>{{ lastReading }} {{ unit }}</BCardText>
+        </BCardBody>
+        <BCardFooter>
+            <BButton class="me-2">Add Reading</BButton>
+            <BButton>Details</BButton>
+        </BCardFooter>
+    </BCard>
+</template>
--- app/src/pages/MetersPage.vue
+++ app/src/pages/MetersPage.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import MeterCard from '@/components/MeterCard.vue';
+
+</script>
+
 <template>
-    Meters page
+    <BContainer>
+        <BRow>
+            <BCol>
+                <h1>Meters</h1>
+            </BCol>
+        </BRow>
+        <BCardGroup deck>
+            <MeterCard></MeterCard>
+            <MeterCard></MeterCard>
+            <MeterCard></MeterCard>
+        </BCardGroup>
+    </BContainer>
 </template>

3b7a0fde4ac11465b609861a089992c50b01f6d4
Oliver Lomp <ol.lomp@gmail.com>
2026-01-28 19:42:59 +0100

    feat: slight meter layout improvements

--- app/src/pages/MetersPage.vue
+++ app/src/pages/MetersPage.vue
@@ -10,17 +10,15 @@ const metersStore = useMetersStore();
     <BContainer>
         <BRow>
             <BCol>
-                <h1>Meters</h1>
+                <h1 class="d-flex justify-content-between my-3">
+                    Meters
+                    <BButton class="ms-5" variant="outline-primary" v-on:click="metersStore.addMeter()">Add Meter</BButton>
+                </h1>
             </BCol>
         </BRow>
-        <BCardGroup deck>
+        <BCardGroup columns>
             <MeterCard v-for="meter in metersStore.meters" :key="meter.id" :meterId="meter.id" :unit="meter.unit"
                 :lastReading="meter.lastReading" :meterName="meter.name" />
         </BCardGroup>
-        <BRow class="mt-5">
-            <BCol>
-                <BButton variant="outline-primary" v-on:click="metersStore.addMeter()">Add Meter</BButton>
-            </BCol>
-        </BRow>
     </BContainer>
 </template>

c59e4d3c8df44164f632c87c588639db15289548
Oliver Lomp <ol.lomp@gmail.com>
2026-01-28 20:26:30 +0100

    feat: add meter modal

--- app/src/pages/MetersPage.vue
+++ app/src/pages/MetersPage.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import MeterCard from '@/components/MeterCard.vue';
+import type { IMeter } from '@/models/meter';
 import { useMetersStore } from '@/stores/meters';
 
 const metersStore = useMetersStore();
+
+const addModalFormData: Partial<IMeter> = {};
+
+function addModalConfirmed() {
+    // TODO: validate?
+    metersStore.addMeter(addModalFormData as IMeter);
+}
 
 </script>
 
@@ -12,7 +20,7 @@ const metersStore = useMetersStore();
             <BCol>
                 <h1 class="d-flex justify-content-between my-3">
                     Meters
-                    <BButton class="ms-5" variant="outline-primary" v-on:click="metersStore.addMeter()">Add Meter</BButton>
+                    <BButton class="ms-5" variant="outline-primary" v-b-modal.add-meter-modal>Add Meter</BButton>
                 </h1>
             </BCol>
         </BRow>
@@ -21,4 +29,21 @@ const metersStore = useMetersStore();
                 :lastReading="meter.lastReading" :meterName="meter.name" />
         </BCardGroup>
     </BContainer>
+
+    <BModal id="add-meter-modal" title="Add Meter" ok-title="Add" v-on:ok="addModalConfirmed()">
+        <BForm>
+            <BFormGroup id="meter-id-form-group" label="Meter identifier" label-for="meter-id-input"
+                description="A unique meter ID, usually printed on the meter itself.">
+                <b-form-input id="meter-id-input" type="text" placeholder="Enter meter id" v-model="addModalFormData.id" required />
+            </BFormGroup>
+            <BFormGroup id="meter-name-form-group" label="Meter name" label-for="meter-name-input"
+                description="A logical name for the meter that helps you recognize it.">
+                <b-form-input id="meter-name-input" type="text" placeholder="Enter meter name" v-model="addModalFormData.name" required />
+            </BFormGroup>
+            <BFormGroup id="meter-unit-form-group" label="Meter unit" label-for="meter-unit-input"
+                description="The unit in which the readings for the meter are stored.">
+                <b-form-input id="meter-unit-input" type="text" placeholder="Enter meter unit" v-model="addModalFormData.unit" required />
+            </BFormGroup>
+        </BForm>
+    </BModal>
 </template>
--- app/src/stores/meters.ts
+++ app/src/stores/meters.ts
@@ -4,13 +4,8 @@ import { defineStore } from 'pinia'
 export const useMetersStore = defineStore('meters', {
   state: () => ({ meters: [] as IMeter[] }),
   actions: {
-    addMeter() {
-      this.meters.push({
-        id: `${this.meters.length + 1}`,
-        name: `Meter ${this.meters.length + 1}`,
-        unit: 'kWh',
-        lastReading: this.meters.length + 1,
-      })
+    addMeter(meter: IMeter) {
+      this.meters.push({...meter})
     },
   },
 })
